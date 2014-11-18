0d71f04688090fe16799cff986cc4785e29cf96a
Thibault Clerice <leponteineptique@gmail.com>
2014-11-18 12:53:50 +0100

    Upgrading Tests

--- tests/Perseids/Tests/IAM/IdP/IdentityProviderTest.php
+++ tests/Perseids/Tests/IAM/IdP/IdentityProviderTest.php
@@ -7,7 +7,7 @@
 		public function testOverrideOptions() {
 			$testURL1 = "http://localhost";
 			$testURL2 = "http://127.0.0.1";
-			$IdP = new IdentityProvider($testURL1);
+			$IdP = new IdentityProvider($testURL1, "Localhost");
 
 			$this->assertEquals($testURL1, $IdP->getUrl());
 			//Should be default url
