7b6e95a25f329c7530f5b963ae83a95e60df0df4
Thibault Clérice <leponteineptique@gmail.com>
2014-11-21 12:15:14 +0100

    Working on new Profile Entity

--- src/Perseids/IAM/Entity/Profile.php
+++ src/Perseids/IAM/Entity/Profile.php
@@ -1,81 +1,370 @@
 <?php
-	namespace Perseids\IAM\BSP;
+	namespace Perseids\IAM\Entity;
 
-	use Perseids\IAM\BSP\Schema;
-	use Perseids\IAM\BSP\Person;
-	use Perseids\IAM\BSP\Instance;
+	use Perseids\IAM\Entity\Abstraction\EntityBase;
+	use Perseids\IAM\Entity\Abstraction\EntityInterface;
+	use Perseids\IAM\Entity\Contact;
 
-	class Profile {
+	use Perseids\IAM\Property\OtherProfile;
+	use Perseids\IAM\Property\InterestExpertise;
+
+	class Profile extends EntityBase implements EntityInterface {
 		/**
-		 * The Namespace used for the xml
-		 * @var string
+		 * The namespace for this node
+		 * @var string 
 		 */
 		protected $namespace = "person";
 
 		/**
-		 * Properties for given profile. 
-		 * @var array
+		 * The URL path endpoint for this object
+		 * @var string 
 		 */
-		protected $properties;
+		protected $path = "person/{{personID}}/profile";
 
 		/**
-		 * UUID for the profile
+		 * The main node name
 		 * @var string
 		 */
-		protected $uuid = null;
+		protected $node = "bambooProfile";
 
 		/**
-		 * Get the array of properties for this profile
-		 * @return array $properties An array of properties
+		 * The main node attributes
+		 * @var string
 		 */
-		public function getProperties() {
-			return $this->properties;
+		protected $nodeAttributes = "person:confidential=\"false\" person:primary=\"true\" xmlns:contacts=\"http://projectbamboo.org/bsp/services/core/contact\" xmlns:person=\"http://projectbamboo.org/bsp/BambooPerson\"";
+
+		/**
+		 * The Contact tied to the profile
+		 * @var Contact
+		 */
+		protected $profileContact;
+
+		/**
+		 * Interest of the users
+		 * @var array
+		 */
+		protected $interest;
+
+		/**
+		 * Expertise of the users
+		 * @var array
+		 */
+		protected $expertise;
+
+		/**
+		 * External affiliations
+		 * @var array
+		 */
+		protected $externalAffiliation;
+
+		/**
+		 * Preferred Language
+		 * @var string
+		 */
+		protected $preferredLanguage;
+
+		/**
+		 * Language used in Scholarship
+		 * @var array
+		 */
+		protected $languageUsedInScholarship;
+
+		/**
+		 * Profile information / note
+		 * @var string
+		 */
+		protected $profileInformation;
+
+		/**
+		 * Other profile informations
+		 * @var array
+		 */
+		protected $otherProfile;
+
+		function __construct() {
+			parent::__construct();
+			$this->addRequired("profileContact");
+		}
+	
+		/**
+		 * Gets the The namespace for this node.
+		 *
+		 * @return string
+		 */
+		public function getNamespace()
+		{
+		    return $this->namespace;
 		}
 
 		/**
-		 * Set the properties for the profile
-		 * @param array $properties An array of properties
-		 * @return \Perseids\IAM\BSP\Profile This instance
+		 * Sets the The namespace for this node.
+		 *
+		 * @param string $namespace the namespace
+		 *
+		 * @return self
 		 */
-		public function setProperties($properties) {
-			$this->properties = $properties;
-			return $this;
+		protected function setNamespace($namespace)
+		{
+		    $this->namespace = $namespace;
+
+		    return $this;
 		}
 
 		/**
-		 * Set the UUID of the profile
-		 * @param string $uuid UUID of the profile
-		 * @return \Perseids\IAM\BSP\Profile This instance
+		 * Gets the The URL path endpoint for this object.
+		 *
+		 * @return string
 		 */
-		public function setUUID($uuid) {
-			$this->uuid = $uuid;
-			return $this;
+		public function getPath()
+		{
+		    return $this->path;
 		}
 
 		/**
-		 * Get the UUID of the profile
-		 * @return string	UUID of the profile
+		 * Sets the The URL path endpoint for this object.
+		 *
+		 * @param string $path the path
+		 *
+		 * @return self
 		 */
-		public function getUUID() {
-			return $this->uuid;
+		protected function setPath($path)
+		{
+		    $this->path = $path;
+
+		    return $this;
 		}
 
 		/**
-		 * Add a property value for this profile.
-		 * @param string $field Field name
-		 * @param string $value Value for a property
-		 * @return \Perseids\IAM\BSP\Profile This instance
+		 * Gets the The main node name.
+		 *
+		 * @return string
 		 */
-		public function addProperty($field, $value) {
-			if(!array_key_exists($field, $this->properties)) {
-				$this->properties[$field] = array();
-			}
-			if(!in_array($value, $this->properties[$field], $strict = TRUE)) {
-				$this->properties[$field][] = $value;	
-			}
-			return $this;
+		public function getNode()
+		{
+		    return $this->node;
 		}
 
+		/**
+		 * Sets the The main node name.
+		 *
+		 * @param string $node the node
+		 *
+		 * @return self
+		 */
+		protected function setNode($node)
+		{
+		    $this->node = $node;
 
-	}
-?>
+		    return $this;
+		}
+
+		/**
+		 * Gets the The main node attributes.
+		 *
+		 * @return string
+		 */
+		public function getNodeAttributes()
+		{
+		    return $this->nodeAttributes;
+		}
+
+		/**
+		 * Sets the The main node attributes.
+		 *
+		 * @param string $nodeAttributes the node attributes
+		 *
+		 * @return self
+		 */
+		protected function setNodeAttributes($nodeAttributes)
+		{
+		    $this->nodeAttributes = $nodeAttributes;
+
+		    return $this;
+		}
+
+		/**
+		 * Gets the The Contact tied to the profile.
+		 *
+		 * @return Contact
+		 */
+		public function getProfileContact()
+		{
+		    return $this->profileContact;
+		}
+
+		/**
+		 * Sets the The Contact tied to the profile.
+		 *
+		 * @param Contact $profileContact the profile contact
+		 *
+		 * @return self
+		 */
+		protected function setProfileContact(Contact $profileContact)
+		{
+		    $this->profileContact = $profileContact;
+
+		    return $this;
+		}
+
+		/**
+		 * Gets the Interest of the users.
+		 *
+		 * @return array
+		 */
+		public function getInterest()
+		{
+		    return $this->interest;
+		}
+
+		/**
+		 * Sets the Interest of the users.
+		 *
+		 * @param array $interest the interest
+		 *
+		 * @return self
+		 */
+		protected function setInterest(array $interest)
+		{
+			return $this->setListOfObject("interest", $interest, "Perseids/IAM/Property/InterestExpertise");
+		}
+
+		/**
+		 * Gets the Expertise of the users.
+		 *
+		 * @return array
+		 */
+		public function getExpertise()
+		{
+		    return $this->expertise;
+		}
+
+		/**
+		 * Sets the Expertise of the users.
+		 *
+		 * @param array $expertise the expertise
+		 *
+		 * @return self
+		 */
+		protected function setExpertise(array $expertise)
+		{
+			return $this->setListOfObject("expertise", $expertise, "Perseids/IAM/Property/InterestExpertise");
+		}
+
+		/**
+		 * Gets the External affiliations.
+		 *
+		 * @return array
+		 */
+		public function getExternalAffiliation()
+		{
+		    return $this->externalAffiliation;
+		}
+
+		/**
+		 * Sets the External affiliations.
+		 *
+		 * @param array $externalAffiliation the external affiliation
+		 *
+		 * @return self
+		 */
+		protected function setExternalAffiliation(array $externalAffiliation)
+		{
+		    $this->externalAffiliation = $externalAffiliation;
+
+		    return $this;
+		}
+
+		/**
+		 * Gets the Preferred Language.
+		 *
+		 * @return string
+		 */
+		public function getPreferredLanguage()
+		{
+		    return $this->preferredLanguage;
+		}
+
+		/**
+		 * Sets the Preferred Language.
+		 *
+		 * @param string $preferredLanguage the preferred language
+		 *
+		 * @return self
+		 */
+		protected function setPreferredLanguage($preferredLanguage)
+		{
+		    $this->preferredLanguage = $preferredLanguage;
+
+		    return $this;
+		}
+
+		/**
+		 * Gets the Language used in Scholarship.
+		 *
+		 * @return array
+		 */
+		public function getLanguageUsedInScholarship()
+		{
+		    return $this->languageUsedInScholarship;
+		}
+
+		/**
+		 * Sets the Language used in Scholarship.
+		 *
+		 * @param array $languageUsedInScholarship the language used in scholarship
+		 *
+		 * @return self
+		 */
+		protected function setLanguageUsedInScholarship(array $languageUsedInScholarship)
+		{
+		    $this->languageUsedInScholarship = $languageUsedInScholarship;
+
+		    return $this;
+		}
+
+		/**
+		 * Gets the Profile information / note.
+		 *
+		 * @return string
+		 */
+		public function getProfileInformation()
+		{
+		    return $this->profileInformation;
+		}
+
+		/**
+		 * Sets the Profile information / note.
+		 *
+		 * @param string $profileInformation the profile information
+		 *
+		 * @return self
+		 */
+		protected function setProfileInformation($profileInformation)
+		{
+		    $this->profileInformation = $profileInformation;
+
+		    return $this;
+		}
+
+		/**
+		 * Gets the Other profile informations.
+		 *
+		 * @return array
+		 */
+		public function getOtherProfile()
+		{
+		    return $this->otherProfile;
+		}
+
+		/**
+		 * Sets the Other profile informations.
+		 *
+		 * @param array $otherProfile the other profile
+		 *
+		 * @return self
+		 */
+		protected function setOtherProfile(array $otherProfile)
+		{
+			return $this->setListOfObject("otherProfile", $otherProfile, "Perseids/IAM/Property/OtherProfile");
+		}
+}
