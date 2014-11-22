d702362c9d1fd6d0865b97e5a5c2ba30c6bcb37d
Thibault Clérice <leponteineptique@gmail.com>
2014-11-22 17:52:51 +0100

    Totally fixing #7 with Person

--- tests/Perseids/Tests/IAM/Entity/PersonTest.php
+++ tests/Perseids/Tests/IAM/Entity/PersonTest.php
@@ -8,21 +8,30 @@
 	class PersonTest extends \PHPUnit_Framework_TestCase {
 		public function testGetXML() {
 
-			$twitter = new IdentityProvider("http://twitter.com", "Twitter", "Twitter-UserUID");
+			$sourceOne = new IdentityProvider("http://someidp.org", "One SourcedId", "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd100126d9069");
+			$sourceTwo = new IdentityProvider("http://someidp.org", "Two SourcedId", "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677264addd100126d9069");
+			
 			$person = new Person();
 			$person
-				->setId(1)
-				->addSourceId($twitter);
+				->addSourceId($sourceOne)
+				->addSourceId($sourceTwo);
 
-			$shouldReturn = '<person:bambooPerson xmlns:person="http://projectbamboo.org/bsp/BambooPerson" xmlns:xs="http://www.w3.org/2001/XMLSchema">
-<person:sourcedId>
-<person:sourcedIdName>Twitter</person:sourcedIdName>
-<person:sourcedIdKey>
-<person:idPId>http://twitter.com</person:idPId>
-<person:userId>Twitter-UserUID</person:userId>
-</person:sourcedIdKey>
-</person:sourcedId>
-</person:bambooPerson>';
+			$shouldReturn = "<person:bambooPerson xmlns:person=\"http://projectbamboo.org/bsp/BambooPerson\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">
+  <person:sourcedId>
+    <person:sourcedIdName>One SourcedId</person:sourcedIdName>
+    <person:sourcedIdKey>
+      <person:idPId>http://someidp.org</person:idPId>
+      <person:userId>7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd100126d9069</person:userId>
+    </person:sourcedIdKey>
+  </person:sourcedId>
+  <person:sourcedId>
+    <person:sourcedIdName>Two SourcedId</person:sourcedIdName>
+    <person:sourcedIdKey>
+      <person:idPId>http://someidp.org</person:idPId>
+      <person:userId>7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677264addd100126d9069</person:userId>
+    </person:sourcedIdKey>
+  </person:sourcedId>
+</person:bambooPerson>";
 
 			$this->assertXmlStringEqualsXmlString($shouldReturn, $person->getXML($attributes = true));
 		}
